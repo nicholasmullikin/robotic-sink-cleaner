d58e2a46bb03470c5b40ebbcf475c0498f325022
Nicholas Mullikin <nicholas.mullikin@gmail.com>
2021-04-01 11:19:02 -0400

    new files

---
+++ custom-data/sink/README.txt
@@ -0,0 +1,8 @@
+Model made by HSDev, modelled in Blender 2.83 (2020)
+
+Thank you for downloading!
+You may use this model and its textures in any of your projects.
+Credit to me would be much obliged, though not required.
+Also leaving your feedback or critique through the comment section would help me a great deal.
+
+Enjoy the model!
--- showPlate.py
+++ showPlate.py
@@ -21,8 +21,13 @@ p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
 #disable tinyrenderer, software (CPU) renderer, we don't use it here
 p.configureDebugVisualizer(p.COV_ENABLE_TINY_RENDERER, 0)
 
+
+#cubeStartPos = [0, 0, 1]
+#cubeStartOrientation = p.getQuaternionFromEuler([90, 0, 0])
+#boxId = p.loadURDF("dinnerware/plate.urdf", cubeStartPos, cubeStartOrientation)
+
 shiftPlate = [0, 0.02, 0]
-meshScalePlate = [.05, .05, .05]
+meshScalePlate = [.02, .02, .02]
 #the visual shape and collision shape can be re-used by all createMultiBody instances (instancing)
 visualShapeId = p.createVisualShape(shapeType=p.GEOM_MESH,
                                     fileName="plate.obj",
@@ -31,36 +36,58 @@ visualShapeId = p.createVisualShape(shapeType=p.GEOM_MESH,
                                     visualFramePosition=shiftPlate,
                                     meshScale=meshScalePlate)
 collisionShapeId = p.createCollisionShape(shapeType=p.GEOM_MESH,
-                                          fileName="plate.obj",
+                                          fileName="custom-data/plate.obj",
                                           collisionFramePosition=shiftPlate,
                                           meshScale=meshScalePlate)
 										  
 shiftSink = [0, 0.02, 0]
 meshScaleSink = [1, 1, 1]									  
 visualShapeId1 = p.createVisualShape(shapeType=p.GEOM_MESH,
-                                    fileName="sink.obj",
+                                    fileName="custom-data/sink.obj",
                                     rgbaColor=[1, 1, 1, 1],
                                     specularColor=[0.4, .4, 0],
                                     visualFramePosition=shiftSink,
-                                    meshScale=meshScaleSink)
+									visualFrameOrientation=p.getQuaternionFromEuler([90,0,0]),
+                                    meshScale=meshScaleSink,
+									)
 collisionShapeId1 = p.createCollisionShape(shapeType=p.GEOM_MESH,
-                                          fileName="sink.obj",
+                                          fileName="custom-data/sink.obj",
                                           collisionFramePosition=shiftSink,
+										  collisionFrameOrientation=p.getQuaternionFromEuler([90,0,0]),
                                           meshScale=meshScaleSink)
 										  
-								  
+	
+shiftDishwasher = [0, 0, 0]
+meshScaleDishwasher = [.2, .2, .2]	
+visualDishwasher = p.createVisualShape(shapeType=p.GEOM_MESH,
+                                    fileName="custom-data/dishwasher/dishwasher.obj",
+                                    rgbaColor=[1, 1, 1, 1],
+                                    specularColor=[0.4, .4, 0],
+                                    visualFramePosition=shiftDishwasher,
+									visualFrameOrientation=p.getQuaternionFromEuler([90,0,0]),
+                                    meshScale=meshScaleDishwasher)
+collisionDishwasher = p.createCollisionShape(shapeType=p.GEOM_MESH,
+                                          fileName="custom-data/dishwasher/dishwasher.obj",
+                                          collisionFramePosition=shiftDishwasher,
+										  collisionFrameOrientation=p.getQuaternionFromEuler([90,0,0]),
+                                          meshScale=meshScaleDishwasher)	
+p.createMultiBody(baseMass=100,
+				  baseInertialFramePosition=[0, 0, 0],
+				  baseCollisionShapeIndex=collisionDishwasher,
+				  baseVisualShapeIndex=visualDishwasher,
+				  basePosition=[0, 0, 0.3],
+				  useMaximalCoordinates=True)
 
-rangex = 1
-rangey = 1
-for i in range(rangex):
-  for j in range(rangey):
-    p.createMultiBody(baseMass=1,
-                      baseInertialFramePosition=[0, 0, 0],
-                      baseCollisionShapeIndex=collisionShapeId1,
-                      baseVisualShapeIndex=visualShapeId1,
-                      basePosition=[((-rangex / 2) + i) * meshScaleSink[0] * 2,
-                                    (-rangey / 2 + j) * meshScaleSink[1] * 2, 1],
-                      useMaximalCoordinates=True)
+
+
+
+
+p.createMultiBody(baseMass=100,
+				  baseInertialFramePosition=[0, 0, 0],
+				  baseCollisionShapeIndex=collisionShapeId1,
+				  baseVisualShapeIndex=visualShapeId1,
+				  basePosition=[-1, 0, 0.3],
+				  useMaximalCoordinates=True)
 
 
 
@@ -70,12 +97,12 @@ rangex = 2
 rangey = 2
 for i in range(rangex):
   for j in range(rangey):
-    p.createMultiBody(baseMass=1,
+    p.createMultiBody(baseMass=20,
                       baseInertialFramePosition=[0, 0, 0],
                       baseCollisionShapeIndex=collisionShapeId,
                       baseVisualShapeIndex=visualShapeId,
-                      basePosition=[((-rangex / 2) + i) * meshScalePlate[0] * 2,
-                                    (-rangey / 2 + j) * meshScalePlate[1] * 2, 1],
+                      basePosition=[-1+i/4,
+                                    0+j/4, 2],
                       useMaximalCoordinates=True)
 					  
 p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
